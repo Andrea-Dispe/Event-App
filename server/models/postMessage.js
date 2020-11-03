f1b7563842ce3d2a60aad5b2024982de98a5956e
Andrea-Dispe <dispe.andrea@gmail.com>
2020-11-04 00:27:21 +0800

    Feat: post messages schema model added

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,1 +1,23 @@
- 
+  const mongoose = require('mongoose');
+  // create a mongoose schema object
+  // the mongoose schema defines what each post must have
+const postSchema = mongoose.Schema({
+  title: String,
+  indexmessage: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    defualt: 0
+  },
+  crdatedAt: {
+    type: Date,
+    default: new Date()
+  }
+
+
+})
+
+exports.PostMessage = mongoose.model('PostMessage', postSchema);
+
