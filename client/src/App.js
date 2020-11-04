38eafda32ca2a7aa11f89440c29e7d5e96e0a1f1
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-11-04 21:31:34 +0800

    Feat: set up the basic folder structure and started to work on the material UI

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react;'
+import './App.css';
+// import the UI component needed from the material UI
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
