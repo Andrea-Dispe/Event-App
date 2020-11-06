9ead05db0ce32fa4d34f12c29ae9601d871a6400
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-11-06 17:08:50 +0800

    Feat: started implementing Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// Redux
+// Provider is going to keep track of the store (a sort of global state)
+// and allows us to access it from anywhere in the app
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <App /> , document.getElementById('root')
